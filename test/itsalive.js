9112d83234576a9a95994939bd4ba8c7593b1ede
JHFellows <j.fellows88@gmail.com>
2017-05-02 12:27:57 -0400

    first commit

---
+++ test/itsalive.js
@@ -0,0 +1,35 @@
+const expect = require("chai").expect;
+var chai = require('chai');
+var spies = require('chai-spies');
+chai.use(spies);
+
+describe("adding function",function(){
+  it("adds 2 + 2",function(){
+    var four = 2 + 2;
+    expect(four).to.equal(4);
+  })
+
+});
+
+describe("setTimeOut function",function(){
+  it("waits for the right amount of time", function(done){
+    var startTime = Date.now();
+    var endTime;
+    setTimeout(function(){
+      endTime = Date.now();
+      //need expect in async function(can't get ref outside)
+      expect(endTime - startTime).to.be.within(900,1100);
+      done();
+    },1000)
+  })
+})
+
+describe("forEach",function(){
+  it("invokes its function once for every element",function(){
+    var arr = [1,2,3,4];
+    var func = function(elem){console.log(elem)};
+    func = chai.spy(func);
+    arr.forEach(func);
+    expect(func).to.have.been.called.exactly(4);
+  })
+})
